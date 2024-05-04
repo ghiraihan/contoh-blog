1ee53d0e9680e972534c8cf0c62f2a0713b9376e
ghiraihan <graihan@binaracademy.org>
2024-05-04 18:49:07 +0700

    done all api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Client } = require('pg');
 const PORT = 3555;
 
 const app = express();
+app.use(express.urlencoded());
 
 // CONFIG DATABASE
 const client = new Client({
@@ -21,6 +22,104 @@ client.connect(function(error) {
 })
 
 // ROUTING
+// 1. GET LIST API
+app.get('/api/article', async function (req, res) {
+  const rawData = await client.query(`
+      SELECT id, title
+      FROM article
+  `);
+  const data = rawData.rows;
+  const count = rawData.rowCount;
+  res.status(200).json({ 
+    count,
+    data
+   })
+});
+// 2. GET DETAIL API
+app.get('/api/article/detail/:id', async function (req, res) {
+  const id = req.params.id;
+  const rawData = await client.query(`
+      SELECT title, body
+      FROM article
+      WHERE id = $1
+  `, [id]);
+  const data = rawData.rows[0];
+  res.status(200).json({ data })
+});
+// 3. INSERT ARTICLE API
+app.post('/api/article/create', async function (req, res) {
+  await client.query('BEGIN');
+  try {
+    const payload = req.body;
+    const title = payload.title;
+    const body = payload.body;
+
+    const rawData = await client.query(`
+        INSERT INTO article (title, body)
+        VALUES ($1, $2)
+        RETURNING *;
+    `, [title, body]);
+    const data = rawData.rows[0];
+
+    await client.query('COMMIT');
+    res.status(200).json({ data });
+  } catch (error) {
+    console.log(error);
+    await client.query('ROLLBACK');
+    res.status(500).send('internal server error');
+  }
+});
+// 4. UPDATE ARTICLE API
+app.put('/api/article/update/:id', async function (req, res) {
+  await client.query('BEGIN');
+  try {
+    const payload = req.body;
+    const id = req.params.id;
+
+    const rawOldData = await client.query(`
+        SELECT title, body
+        FROM article
+        WHERE id = $1
+    `, [id]);
+    const oldData = rawOldData.rows[0];
+
+    const title = payload.title || oldData.title;
+    const body = payload.body || oldData.body;
+
+    const newRawData = await client.query(`
+        UPDATE article
+        SET title = $1, body = $2
+        WHERE id = $3
+        RETURNING *
+     `, [title, body, id]);
+    const newData = newRawData.rows[0];
+
+    await client.query('COMMIT');
+    res.status(200).json({ data: newData })
+  } catch (error) {
+    console.log(error);
+    await client.query('ROLLBACK');
+    res.status(500).send('internal server error');
+  }
+});
+
+// 5. Delete article api
+app.delete('/api/article/delete/:id',async function (req, res) {
+  await client.query('BEGIN');
+  try {
+    const id = req.params.id;
+    await client.query(`
+        DELETE FROM article
+        WHERE id = $1
+    `, [id]);
+    await client.query('COMMIT');
+    res.status(200).json({ id });
+  } catch (error) {
+    console.log(error);
+    await client.query('ROLLBACK');
+    res.status(500).send('internal server error');
+  }
+})
 
 app.listen(PORT, function () {
   console.log(`Server berjalan di http://localhost:${PORT}`);
